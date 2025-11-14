92c054c3cca353705c6aa89c80397c8b9299e82d
Vu Phuc Quang Vinh <vinhvpq2004@gmail.com>
2025-11-14 13:23:31 +0700

    Update App component to implement tour management system and enhance UI

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,31 +4,86 @@
 import './App.css'
 
 function App() {
+    const tours = [
+        {
+            id: 1,
+            title: 'Tour du lich Bac Kinh - Thuong Hai - Thanh Do xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A',
+            image: 'https://pystravel.vn/_next/image?url=https%3A%2F%2Fbooking.pystravel.vn%2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75'
+        },
+        {
+            id: 2,
+            title: 'Tour du lich Bac Kinh - Thuong Hai - Thanh Do xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A',
+            image: 'https://pystravel.vn/_next/image?url=https%3A%2F%2Fbooking.pystravel.vn%2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75'
+        },
+        {
+            id: 3,
+            title: 'Tour du lich Bac Kinh - Thuong Hai - Thanh Do xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A',
+            image: 'https://pystravel.vn/_next/image?url=https%3A%2F%2Fbooking.pystravel.vn%2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75'
+        },
+        {
+            id: 4,
+            title: 'Tour du lich Bac Kinh - Thuong Hai - Thanh Do xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A',
+            image: 'https://pystravel.vn/_next/image?url=https%3A%2F%2Fbooking.pystravel.vn%2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75'
+        }
+    ]
+    
     // JSX : HTML + JS nam trong return boc trong 1 the bat ky <></>
-    const name = "Nguyen Van Anh";
-    const studenID = "B20DCCN001";
-    const major = "Internet Technology";
-    const age = 20;
     return (
         <div className="App">
-            <h1 className="text-3xl font-bold text-blue-600">Xin Chao{name}</h1>
-            <p className="text-gray-700 text-2xl">{studenID}</p>
-            <p className="text-gray-700 text-2xl">{major}</p>
-            <h2>
-                Thong tin sinh vien
+            <h1 className="text-3xl font-bold text-blue-600">
+                Tour Management System
+            </h1>
+            <h2 className="uppercase text-center text-2xl font-semibold">
+                Kham pha san pham vietravel
             </h2>
-            <p>
-                <strong>Ho ten :</strong>{name}
+            <p className="text-md font-medium text-center">
+                Tan huong trai nghiem du lich tuyet voi cung Vietravel tren moi neo duong di . 
+                Mang lai cho ban nhung giac nghi tuyet voi va nhung ky niem khong the nao quen. Giup ban kham 
+                pha the gioi mot cach tron ven nhat.
             </p>
-            <p>
-                <strong>Ma so sinh vien :</strong> {studenID}
-            </p>
-            <p>
-                <strong>Chuyen nganh :</strong> {major}
-            </p>
-            <p>
-                Tuoi : {age +1 } ;
-            </p>
+            <div className="flex gap-2 my-4">
+                {tours.map(tour =>(
+                <div key={tour.id}>
+                    <img src={tour.image} alt={tour.title} width={500} />
+                    <h3 className="font-semibold">{tour.title}</h3>
+            </div>
+                ))}
+                {/* <div>
+                    <img src="https://pystravel.vn/
+                    _next/image?url=https%3A%2F%2Fbooking.pystravel.vn
+                    %2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75"></img>
+                    <p>
+                        Tour du lich Bac Kinh - Thuong Hai - Thanh Do
+                        xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A
+                    </p>
+                </div>
+                <div>
+                    <img src="https://pystravel.vn/
+                    _next/image?url=https%3A%2F%2Fbooking.pystravel.vn
+                    %2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75"></img>
+                    <p>
+                        Tour du lich Bac Kinh - Thuong Hai - Thanh Do
+                        xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A
+                    </p>
+                </div>
+                <div>
+                    <img src="https://pystravel.vn/
+                    _next/image?url=https%3A%2F%2Fbooking.pystravel.vn
+                    %2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75"></img>
+                    <p>
+                        Tour du lich Bac Kinh - Thuong Hai - Thanh Do
+                        xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A
+                    </p>
+                </div>
+                <div>
+                    <img src="https://pystravel.vn/
+                    _next/image?url=https%3A%2F%2Fbooking.pystravel.vn
+                    %2Fuploads%2Fposts%2Favatar%2F1759371709.jpg&w=640&q=75"></img>
+                    <p>
+                        Tour du lich Bac Kinh - Thuong Hai - Thanh Do
+                        xem CKTG giua T1 - KT Rolster gia 20 trieu bao gom ca ve xem hang A
+                    </p> */}
+               </div>
             </div>
     );
 }
