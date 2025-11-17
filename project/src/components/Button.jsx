28cd58fa69c1daa89f98508f3cb6d05c96bf6c2d
Vu Phuc Quang Vinh <vinhvpq2004@gmail.com>
2025-11-17 12:47:15 +0700

    Add click handler to Button component for tour booking alert

--- project/src/components/Button.jsx
+++ project/src/components/Button.jsx
@@ -1,9 +1,12 @@
 function Button(props) { 
     return (
-        <div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                {props.label}</button>
-        </div>
-    )
+        const dattour = () => {
+            alert(`Ban da dat tour: ${props.label}`);
+        }
+        return (
+            <button onClick={dat} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                {props.label}
+            </button>
+        )
 }
 export default Button;
