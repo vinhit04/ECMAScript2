aaa58a090479b78794d8ed5e898f8498ae3391f0
Vu Phuc Quang Vinh <vinhvpq2004@gmail.com>
2025-11-21 12:28:07 +0700

    Add state management to About component for tour tracking

--- project/src/pages/About.jsx
+++ project/src/pages/About.jsx
@@ -1,5 +1,8 @@
-
-function About() {
+import { useState } from 'react';
+ function About() {
+    const [isOn , setIsOn] = useState(false);
+     const [tours, setTour] = useState([]);
+     console.log({ isOn, tours });
     return (
         <div>
             <h1>About Page</h1>
